3a15e9ec03a9b44df4eacb6133bde30e0654d03e
丁敬 <dingjing@live.cn>
2018-04-04 10:48:42 +0800

    加入天购买量和天购买留存

--- InjectRetentionRate/src/com/easou/retention/InjectRetentAndBuyDay.java
+++ InjectRetentionRate/src/com/easou/retention/InjectRetentAndBuyDay.java
@@ -9,23 +9,21 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 
-
-public class InjectRetentionZero {
-
+public class InjectRetentAndBuyDay {
 	protected static Configuration hBaseConfiguration = null;
     protected static HTable hTable = null;
     protected static FileWriter fileWriter = null;
     protected static List<Put> list = null;
     protected static int retentNum = 0;
-    protected static int readNum = 0;
+    protected static int buyNum = 0;
     protected static String logFile = null;
 	    
-    static {        
+    static {
 	    hBaseConfiguration = HBaseConfiguration.create();
 	    hBaseConfiguration.set("hbase.zookeeper.quorum", "moses.namenode01,moses.datanode10,moses.datanode11,moses.datanode12,moses.datanode13");
 	    hBaseConfiguration.set("hbase.zookeeper.property.clientPort", "2181");
@@ -33,12 +31,10 @@ public class InjectRetentionZero {
     }
 	    
     static HTable getHtable(String tableName) {
-
     	try {
     		hTable = new HTable(hBaseConfiguration, tableName);
     	} catch (IOException e) {
     		e.printStackTrace();
-    		
     		System.out.println("错误");
     		return null;
     	}
@@ -50,10 +46,10 @@ public class InjectRetentionZero {
         Put e = new Put(Bytes.toBytes(row));
         e.add(Bytes.toBytes(columnfamily), Bytes.toBytes(column), Bytes.toBytes(value));
         list.add(e);
+        //System.out.println("添加!!!");
     }
 	    
     static void commitHbase() {
-    	
     	if (null == hTable) {
     		System.out.println("htable 错误!!!");
     		return;
@@ -80,13 +76,12 @@ public class InjectRetentionZero {
 			}
     	}
     }
-	    
     
     /**
      *	path 尧都区的 
      * 
      */
-    static void inject(String path, String field1, String field2) {
+    static void injectRetent(String path) {
     	BufferedReader fR = null;
     	try {
     		String lineTemp;
@@ -95,24 +90,74 @@ public class InjectRetentionZero {
     			String[] lineArray = lineTemp.split("\t");
     			String key;
     			String retentTemp;
-    			String readTemp;
+    			float retentF;
     			// gid 和 留存率 
     			try {
-    				if(lineArray.length != 1) {
-    					writeLog(lineTemp + "\twrong length", logFile);
+    				if(lineArray.length != 8) {
+    					writeLog("inject retent:" + lineTemp + "\twrong length", logFile);
     					continue;
     				}
     				
     				key = lineArray[0];
-    				retentTemp = "0";
-    				readTemp = "0";
+    				retentTemp = lineArray[5];
     				
-    				addRow(hTable, key, "x", field1, retentTemp);
-    				addRow(hTable, key, "x", field2, readTemp);
-    				++ retentNum;
-    				++ readNum;
+    				retentF = Float.parseFloat(retentTemp);
+    				if (retentF > 0 && retentF <= 1) {
+        				addRow(hTable, key, "x", "bt_d", retentTemp);
+        				++ retentNum;
+    				}
 
 					if(list.size() > 4096) {
+						commitHbase();
+					}
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					commitHbase();
+				}
+    		}
+    		commitHbase();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	} finally {
+    		if (fR != null) {
+    			try {
+					fR.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}
+    	}
+    }
+    
+    static void injectBuyNum(String path) {
+    	BufferedReader fR = null;
+    	try {
+    		String lineTemp;
+    		fR = new BufferedReader(new FileReader(new File(path)));
+    		while ((lineTemp = fR.readLine()) != null) {
+    			String[] lineArray = lineTemp.split("\t");
+    			String key;
+    			String buyTemp;
+    			int buyI;
+    			// gid 和 阅读量 
+    			try {
+    				if(lineArray.length != 2) {
+    					writeLog("inject num: " + lineTemp + "\twrong length", logFile);
+    					continue;
+    				}
+    				
+    				key = lineArray[0];
+    				buyTemp = lineArray[1];
+    				buyI = Integer.parseInt(buyTemp);
+    				if (buyI > 0) {
+    					addRow(hTable, key, "x", "bn_d", buyTemp);
+    					++ buyNum;
+    				}
+
+					if(list.size() > 8192) {
 						commitHbase();
 					}
 				} catch (Exception e) {
@@ -154,50 +199,35 @@ public class InjectRetentionZero {
 			e.printStackTrace();
 		}
     }
-    
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		if(args.length != 5) {
-			System.out.println("输入参数错误:\n请依次输入:留存率结果、日志文件、hbase表名、字段1、字段2");
+		if(args.length != 4) {
+			System.out.println("输入参数错误:\n请依次输入:留存率结果、购买量结果、日志文件、hbase表名");
 			
 			return;
 		}
 
 		String retentionPath = args[0];
-		logFile = args[1];
-		String tableName = args[2];
-		String field1 = args[3];
-		String field2 = args[4];
+		String buyNumPath = args[1];
+		logFile = args[2];
+		String tableName = args[3];
 		
-		System.out.println("inject file: " + retentionPath);
+		System.out.println("bt_d file: " + retentionPath);
+		System.out.println("bn_d file: " + buyNumPath);
 		System.out.println("log path: " + logFile);
 		System.out.println("hbase table name: " + tableName);
-		System.out.println("field1: " + field1);
-		System.out.println("field2: " + field2);
-		
-		
+	
 		// 获取 hbase 的 htable;
 		hTable = getHtable(tableName); //-----
 		writeLog("start inject...", logFile);
-		
-		/* 检查字段是否合法 */
-		if((!field1.equals("rn_d") || !field2.equals("rt_d"))
-				&& (!field1.equals("rn_w") || !field2.equals("rt_w"))
-				&& (!field1.equals("rn_w7") || !field2.equals("rt_w7"))
-				&& (!field1.equals("bn_d") || !field2.equals("bt_d"))) {
-			System.out.println("输入hbase字段出错(或顺序出错)");
-			writeLog("输入hbase字段出错(或顺序出错)", logFile);
-			return;
-		}
-		
-		inject(retentionPath, field1, field2);
-		writeLog("留存率写入数量：" + retentNum, logFile);
-		writeLog("阅读量/购买量写入数量：" + readNum, logFile);
+		injectRetent(retentionPath);
+		injectBuyNum(buyNumPath);
+		writeLog("购买留存率写入数量：" + retentNum, logFile);
+		writeLog("购买量写入数量：" + buyNum, logFile);
 		closeHbase();
 		writeLog("inject complete!!!", logFile);
-		
 		System.out.println("ok");
 	}
 
